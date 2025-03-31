08b6c6850968631486b3868eea5078874622b5e7
arthurmeb <120094553+arthurmeb@users.noreply.github.com>
2025-03-31 19:36:15 +0100

    good enogugh to start

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1> YO this a h1 big boy</h1>
+    <button class="btn lg">This a daisy button</button>
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
